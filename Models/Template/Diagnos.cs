eee73762b80aa8f63cca2f5d5ecc63081f2ed57f
Alexander Balabin <balabin@ymail.com>
2021-01-15 14:16:53 +0400

    добавил поиск в дереве

--- Models/Template/Diagnos.cs
+++ Models/Template/Diagnos.cs
@@ -46,33 +46,7 @@ namespace Dental.Models.Template
             {
                 Diagnos model = (Diagnos)p;
 
-                if (model.Id == model.ParentId && model.Dir == 0)
-                {
-                    // это корневой файл
-                    // показываем форму с выбором корневой и обычной папки и файла
-                    int x = 0;
-                }
-                if (model.Id == model.ParentId && model.Dir == 1)
-                {
-                    // это корневая папка 
-                    // показываем форму с выбором корневой и обычной папки и файла
-                    int x = 0;
-                }
-
-                else if (model.Dir == 1 && model.ParentId != model.Id)
-                {
-                    // это вложенная папка, 
-                    // показываем форму с выбором файла и обычной папки, присваиваем записи ParentId = текущему Id
-                    int x = 0;
-                }
-
-
-                if (model.Dir == 0 && model.ParentId != 0)
-                {
-                    // это файл в папке
-                    // показываем форму с выбором файла и обычной папки, присваиваем записи ParentId = текущему ParentId
-                    int x = 0;
-                }
+                
 
             }
             catch (Exception e)
