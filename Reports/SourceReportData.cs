230154b9ad553d16be21c0717055577bbfff4b1c
Alexander Balabin <balabin@ymail.com>
2021-05-12 23:28:59 +0400

    привязка отчета к объекту данных

---
+++ Reports/SourceReportData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.DataAccess.ObjectBinding;
+using System.ComponentModel;
+
+namespace Dental.Reports
+{
+    public class SourceReportData
+    {
+        public string DocumentSource { get; set; }
+        public string OrganizationName { get; set; } = "dsadadasds";
+        public string PatientFullNameName { get; set; }
+        public string EmployeeFullNameName { get; set; }
+    }
+
+    [DisplayName("ReportData")]
+    [HighlightedClass]
+    public class DataSource
+    {
+
+        private SourceReportData data;
+        public DataSource(SourceReportData source)
+        {
+            data = source;
+        }
+
+        [HighlightedMember]
+        public SourceReportData GetData()
+        {
+            return data; 
+        }
+    }
+
+
+}
--- ViewModels/IdsViewModel.cs
+++ ViewModels/IdsViewModel.cs
@@ -18,6 +18,10 @@ using Dental.Reports;
 using DevExpress.XtraReports.UI;
 using DevExpress.Xpf.Printing;
 using System.Windows;
+using DevExpress.XtraReports.Parameters;
+using DevExpress.XtraReports.Expressions;
+using DevExpress.DataAccess.ObjectBinding;
+using System.Drawing;
 
 namespace Dental.ViewModels
 {
@@ -25,7 +29,7 @@ namespace Dental.ViewModels
     {
         public IdsViewModel()
         {
-            OpenCommand = new LambdaCommand(OnOpenCommandExecuted, CanOpenCommandExecute);      
+            OpenCommand = new LambdaCommand(OnOpenCommandExecuted, CanOpenCommandExecute);
         }
 
         public ICommand OpenCommand { get; }
@@ -37,7 +41,38 @@ namespace Dental.ViewModels
         {
             try
             {
-                var report = new XtraReport2();
+                var data = new SourceReportData() { 
+                    OrganizationName = "Елки - палки",
+                    PatientFullNameName = "Иванов Иван Иванович",
+                    EmployeeFullNameName = "Петров Петр Петрович"
+                };
+
+                var report = new XtraReport1();
+                ObjectDataSource objectDataSource = new ObjectDataSource();
+                objectDataSource.Name = "Report";
+                objectDataSource.DataSource = new DataSource(data);
+                objectDataSource.BeginUpdate();
+                objectDataSource.DataMember = "GetData";
+
+                objectDataSource.EndUpdate();
+                objectDataSource.Fill();
+                report.DataSource = objectDataSource;
+
+
+
+
+
+
+
+
+                // var s = new Dental.Reports.SourceReportData();
+               
+               /* DevExpress.DataAccess.ObjectBinding.ObjectDataSource objds = new DevExpress.DataAccess.ObjectBinding.ObjectDataSource();
+                objds.Name = "ObjectDataSource1";
+                objds.DataSource = typeof(SourceReportData);
+                objds.Fill();*/
+
+
                 PrintHelper.ShowPrintPreview(new Window(), report);
             }
             catch (Exception e)
@@ -45,18 +80,6 @@ namespace Dental.ViewModels
                 (new ViewModelLog(e)).run();
             }
         }
-
-        public string DocumentSource { get; set; }
-        public string OrganizationName { get; set; }
-        public string PatientFullNameName { get; set; }
-        public string EmployeeFullNameName { get; set; }
-
-        public string employeeFullNameName;
-
-
-
-
-
-
     }
 }
+
