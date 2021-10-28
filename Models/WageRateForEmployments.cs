bd9976588b3065195eadfdd5ee8f5a558d4668a0
Alexander Balabin <balabin@ymail.com>
2021-10-28 13:38:18 +0400

    сетка для тарифов

--- Models/WageRateForEmployments.cs
+++ Models/WageRateForEmployments.cs
@@ -15,17 +15,20 @@ namespace Dental.Models
         [Display(Name = "Название")]
         public string Name { get; set; }
 
+        [Display(Name = "Активен")]
+        public int IsActive { get; set; } = 1;
+
         [Display(Name = "Является базовым")]
         public int IsBasic { get; set; } = 0;       
         
         [Display(Name = "Применяется правило")]
         public int IsApplyRule { get; set; } = 0;
 
+        [Display(Name = "Больше или меньше базового")]
+        public string MoreOrLess { get; set; }
+
         [Display(Name = "Процент или значение")] // 10% или 500 руб.
         public string PercentOrCost { get; set; }
-
-        [Display(Name = "Значение")]
-        public string Value { get; set; }
 
         public string Error { get => string.Empty; }
         public string this[string columnName] { get => IDataErrorInfoHelper.GetErrorText(this, columnName); }
