2a32c900cdb0db72bdfaf711561374446b5b0db6
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2022-04-09 13:59:31 +0400

    локация встреч в расписании, при закрытии окна, отслеживать несохраненные изменения

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,9 +37,7 @@ namespace Dental
             finally
             {
                 Application.Current.Shutdown();
-            }
-            
-            
+            }                    
         }
 
     }
--- Views/WindowForms/LocationAppointmentWindow.xaml.cs
+++ Views/WindowForms/LocationAppointmentWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using Dental.Models;
+using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 
 namespace Dental.Views.WindowForms
 {
@@ -7,6 +13,54 @@ namespace Dental.Views.WindowForms
         public LocationAppointmentWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                
+                if (((ViewModels.ShedulerViewModel)((FrameworkElement)sender).DataContext).LocationAppointments is ObservableCollection<LocationAppointment> collection)
+                {
+                    bool isHaveUnsavedChange = false;
+                    if (collection?.Count > 0)
+                    {
+                        // ищем несохраненные позиции
+                        foreach (var item in collection)
+                        {
+                            if (item.Id == 0) isHaveUnsavedChange = true;
+                        }
+                    }
+
+                    if (isHaveUnsavedChange)
+                    {
+                        string mes = "Имеются несохраненные позиции в списке! Закрыть форму?";
+                        var response = ThemedMessageBox.Show(title: "Внимание", text: mes, messageBoxButtons: MessageBoxButton.YesNo, icon: MessageBoxImage.Warning);
+
+                        if (response.ToString() == "No")
+                        {
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                    // удаляем несохраненные позиции
+                    // заводим коллекцию
+                    var forRemoving = new ConcurrentBag<LocationAppointment>();
+                    foreach (var item in collection)
+                    {
+                        // складываем в нее элементы подлежащие удалению
+                        if (item.Id == 0) forRemoving.Add(item);
+                    }
+                    // удаляем
+                    foreach (var item in forRemoving) collection.Remove(item);
+
+                }               
+                e.Cancel = false;
+            }
+            catch
+            {
+                e.Cancel = false;
+            }
         }
     }
 }
