8db3ce27e4494a0861fb3d5e00e41fb110572b08
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2022-05-03 12:44:17 +0400

    форма позиции услуг в смете

--- ViewModels/Estimates/EstimatesViewModel.cs
+++ ViewModels/Estimates/EstimatesViewModel.cs
@@ -155,16 +155,161 @@ namespace Dental.ViewModels.Estimates
                 (new ViewModelLog(e)).run();
             }
         }
-
-        public ICollection<Estimate> Estimates { get; set; }
-        public ICollection<Employee> Employees { get; set; }
-        public EstimateVM EstimateVM { get; set; }
-        public Estimate Estimate { get; set; }
-        public Client Client { get; set; }
-        private EstimateWindow EstimateWindow;
         #endregion
 
 
+        #region Состав сметы
+        [Command]
+        public void SelectItemInServiceField(object p)
+        {
+            try
+            {
+                if (p is FindCommandParameters parameters)
+                {
+                    if (parameters.Tree.FocusedRow is Service service)
+                    {
+                        if (service.IsDir == 1) return;
+                        parameters.Popup.EditValue = service;
+                    }
+                    parameters.Popup.ClosePopup();
+                }
+            }
+            catch (Exception e)
+            {
+                (new ViewModelLog(e)).run();
+            }
+        }
+
+        [Command]
+        public void OpenFormEstimateService(object p)
+        {
+            try
+            {
+                Employees = db.Employes.OrderBy(f => f.LastName).ToArray();
+                Services = db.Services.ToArray();
+                if (p is Estimate estimate)
+                {
+                    EstimateServiceItemVM = new EstimateServiceItemVM() { Estimate = estimate, Title = "Добавление новой услуги" };
+                    EstimateServiceWindow = new EstimateServiceWindow() { DataContext = this };
+                    EstimateServiceWindow.ShowDialog();
+                }
+            }
+            catch (Exception e)
+            {
+                (new ViewModelLog(e)).run();
+            }
+        }
+
+        [Command]
+        public void EditEstimateService(object p)
+        {
+            try
+            {
+                if (p is EstimateServiceItem item)
+                {
+                    Employees = db.Employes.OrderBy(f => f.LastName).ToArray();
+                    Services = db.Services.ToArray();
+                    EstimateServiceItemVM = new EstimateServiceItemVM() 
+                    { 
+                        Estimate = item.Estimate, 
+                        Employee = item.Employee, 
+                        Service = item.Service, 
+                        Count = item.Count, 
+                        Price = item.Price,
+                        Title = "Редактирование услуги"
+                    };
+                    EstimateServiceWindow = new EstimateServiceWindow() { DataContext = this };
+                    EstimateServiceWindow.ShowDialog();
+                }
+            }
+            catch (Exception e)
+            {
+                (new ViewModelLog(e)).run();
+            }
+        }
+
+        [Command]
+        public void SaveRowInEstimate(object p)
+        {
+            try
+            {
+
+                if (EstimateServiceItem.Id == 0)
+                {
+                   // Model.Estimates.Where(f => f.Id == EstimateServiceItem.EstimateId).FirstOrDefault().EstimateServiseItems.Add(EstimateServiceItem);
+                }
+
+                //if (db.SaveChanges() > 0) EstimateServiceItemClone = EstimateServiceItem;
+            }
+            catch (Exception e)
+            {
+                (new ViewModelLog(e)).run();
+            }
+            finally
+            {
+                EstimateServiceWindow?.Close();
+            }
+        }
+
+        [Command]
+        public void DeleteEstimateService(object p)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(EstimateServiceItem["Classificator"]))
+                {
+                    int x = 0;
+                }
+                if (p is EstimateServiceItem item)
+                {
+                    var response = ThemedMessageBox.Show(title: "Внимание!", text: "Удалить позицию в смете?", messageBoxButtons: MessageBoxButton.YesNo, icon: MessageBoxImage.Warning);
+                    if (response.ToString() == "No") return;
+
+                    db.Entry(item).State = EntityState.Deleted;
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                (new ViewModelLog(e)).run();
+            }
+        }
+
+        [Command]
+        public void CancelFormEstimateItem(object p)
+        {
+            try
+            {
+               /* if (!string.IsNullOrEmpty(EstimateServiceItem.Service?.Name) && !EstimateServiceItem.Equals(EstimateServiceItemClone))
+                {
+                    EstimateServiceItem = (EstimateServiceItem)EstimateServiceItemClone.Clone();
+                }*/
+                if (p is System.ComponentModel.CancelEventArgs arg)
+                {
+                    arg.Cancel = false;
+                    return;
+                }
+                EstimateServiceWindow?.Close();
+            }
+            catch (Exception e)
+            {
+                (new ViewModelLog(e)).run();
+            }
+
+        }
+
+        public ICollection<Estimate> Estimates { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+        public ICollection<Measure> Measuries { get; set; }
+        public ICollection<Service> Services { get; set; }
+        public EstimateVM EstimateVM { get; set; }
+        public EstimateServiceItemVM EstimateServiceItemVM { get; set; }
+        public Estimate Estimate { get; set; }
+        public EstimateServiceItem EstimateServiceItem { get; set; }
+        public Client Client { get; set; }
+        private EstimateWindow EstimateWindow;
+        public EstimateServiceWindow EstimateServiceWindow;
+        #endregion
     }
 }
 
--- Views/Estimates/EstimateMaterialWindow.xaml.cs
+++ Views/Estimates/EstimateMaterialWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Dental.Views.PatientCard
+namespace Dental.Views.Estimates
 {
     /// <summary>
     /// Логика взаимодействия для EstimateMaterialWindow.xaml
