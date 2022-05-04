1e41696d541683ae7a1bd71e440536f107b8bc8e
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2022-05-05 00:33:00 +0400

    раздел смет

--- Views/Estimates/EstimatesPage.xaml.cs
+++ Views/Estimates/EstimatesPage.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Dental.Models;
+using Dental.ViewModels;
+using DevExpress.Xpf.Grid;
 
 namespace Dental.Views.Estimates
 {
@@ -25,7 +28,55 @@ namespace Dental.Views.Estimates
             InitializeComponent();
         }
 
-        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        private void TreatmentPlanItems_CustomSummary(object sender, DevExpress.Data.CustomSummaryEventArgs e)
+        {
+            if (((GridSummaryItem)e.Item).FieldName == "Price" && e.SummaryProcess == DevExpress.Data.CustomSummaryProcess.Finalize)
+            {
+                if (e.Row == null) return;
+                var items = ((EstimateServiceItem)e.Row)?.Estimate?.EstimateServiseItems;
+                decimal price = 0;
+                foreach (var item in items)
+                {
+                    if (decimal.TryParse(item.Service?.Price?.ToString(), out decimal result))
+                    {
+                        if (item.Count > 0) price += (result * item.Count);
+                    }
+                }
+                e.TotalValue = price;
+            }
+            e.TotalValueReady = true;
+        }
+
+        private void attachFileTableView_CustomRowAppearance(object sender, CustomRowAppearanceEventArgs e)
+        {
+            if (e.Source.DataControl is GridControl dataControl)
+            {
+                e.Handled = true;
+                dataControl.UpdateTotalSummary();
+                return;
+            }
+        }
+
+        private void MaterialItems_CustomSummary(object sender, DevExpress.Data.CustomSummaryEventArgs e)
+        {
+            if (((GridSummaryItem)e.Item).FieldName == "Price" && e.SummaryProcess == DevExpress.Data.CustomSummaryProcess.Finalize)
+            {
+                if (e.Row == null) return;
+                var items = ((EstimateMaterialItem)e.Row)?.Estimate?.EstimateMaterialItems;
+                decimal price = 0;
+                foreach (var item in items)
+                {
+                    if (decimal.TryParse(item.Nomenclature?.Price?.ToString(), out decimal result))
+                    {
+                        if (item.Count > 0) price += (result * item.Count);
+                    }
+                }
+                e.TotalValue = price;
+            }
+            e.TotalValueReady = true;
+        }
+
+        private void grid_Loaded(object sender, RoutedEventArgs e)
         {
 
         }
