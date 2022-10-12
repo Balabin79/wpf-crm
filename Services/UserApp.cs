fb7a13cd390e1b15878aa5d64d7ac0a9ad03735b
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2022-10-12 22:57:11 +0400

    хранение версии и лицензии

---
+++ Services/UserApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental.Services
+{
+    public class UserApp
+    {
+        public string Version = "1.0";
+        public string Lic { get; set; }
+    }
+}
