f48babf404d52587813d996a8b679538b61f1ec5
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2023-01-10 15:31:06 +0400

    шаблоны

---
+++ Views/Templates/Anamneses.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Dental.Models;
+using Dental.Models.Templates;
+using Dental.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dental.Views.Templates
+{
+    public partial class Anamneses : UserControl
+    {
+        public Anamneses()
+        {
+            InitializeComponent();
+            var db = new ApplicationContext();
+            DataContext = new TreeBaseViewModel<Anamnes>(db, db?.Anamneses);
+        }
+    }
+}
