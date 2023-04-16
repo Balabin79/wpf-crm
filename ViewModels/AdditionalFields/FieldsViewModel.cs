6034009c872e1e8d2fc5cef3cad48cdd927d61d2
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2023-04-16 23:06:59 +0400

    поправил дополнительные поля

--- ViewModels/AdditionalFields/FieldsViewModel.cs
+++ ViewModels/AdditionalFields/FieldsViewModel.cs
@@ -36,24 +36,25 @@ namespace B6CRM.ViewModels.AdditionalFields
         public void ClientFieldsLoading(Client client)
         {
             Fields = new ObservableCollection<LayoutItem>();
-            var AdditionalClientFields = db.AdditionalClientFields.Include(f => f.TypeValue).OrderBy(f => f.Sort).ToArray();
-            if (AdditionalClientFields.Count() == 0) return;
+            var additionalClientFields = db.AdditionalClientFields.Include(f => f.TypeValue).OrderBy(f => f.Sort).ToArray();
+
+            if (additionalClientFields.Count() == 0) return;
 
             // загружаем значения полей
             var AdditionalClientValues = client != null ? db.AdditionalClientValue?.Where(f => f.ClientId == client.Id)?.ToObservableCollection() ?? new ObservableCollection<AdditionalClientValue>() : new ObservableCollection<AdditionalClientValue>();
 
-            foreach (var i in AdditionalClientFields)
+            foreach (var i in additionalClientFields)
             {
-                var value = AdditionalClientValues.FirstOrDefault(f => f.AdditionalFieldId == i.Id && f.ClientId == client.Id);
+                var model = AdditionalClientValues.FirstOrDefault(f => f.AdditionalFieldId == i.Id && f.ClientId == client.Id);
                 var binding = new Binding()
-                {
+                {                  
                     Source = this,
                     Path = new PropertyPath("IsReadOnly"),
                     Mode = BindingMode.TwoWay,
                     UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
                 };
 
-                var el = GetField(i?.TypeValue?.SysName, value?.Value);
+                var el = GetField(i?.TypeValue?.SysName, i?.SysName, model);
                 el.SetBinding(BaseEdit.IsReadOnlyProperty, binding);
 
                 var label = new LayoutItem()
@@ -67,36 +68,38 @@ namespace B6CRM.ViewModels.AdditionalFields
             }
         }
 
-        private BaseEdit GetField(string sysName, string value)
+        private BaseEdit GetField(string sysType, string sysName, AdditionalClientValue model)
         {
             try
             {
-                switch (sysName)
+                switch (sysType)
                 {
                     case "string":
-                        return new TextEdit() { EditValue = value };
+                        return new TextEdit() { EditValue = model?.Value, Name = sysName };
 
                     case "money":
                         return new TextEdit()
                         {
+                            Name = model?.AdditionalField?.SysName,
                             Mask = "c",
                             MaskType = MaskType.Numeric,
                             MaskCulture = CultureInfo.CurrentCulture,
                             MaskUseAsDisplayFormat = true,
                             DisplayFormatString = "{}{0:c2}",
-                            EditValue = decimal.TryParse(value, out decimal val) ? string.Format("{0:c2}", val) : value
+                            EditValue = decimal.TryParse(model?.Value, out decimal val) ? string.Format("{0:c2}", val) : model?.Value
                         };
 
                     case "int":
-                        return new TextEdit() { Mask = "d", MaskType = MaskType.Numeric, EditValue = value };
+                        return new TextEdit() { Mask = "d", MaskType = MaskType.Numeric, EditValue = model?.Value, Name = sysName };
 
                     case "float":
-                        return new TextEdit() { Mask = "f", MaskType = MaskType.Numeric, EditValue = value };
+                        return new TextEdit() { Mask = "f", MaskType = MaskType.Numeric, EditValue = model?.Value, Name = sysName };
 
                     case "date":
-                        var dt = DateTime.TryParse(value, out DateTime dateTime) ? dateTime.ToShortDateString() : value;
+                        var dt = DateTime.TryParse(model?.Value, out DateTime dateTime) ? dateTime.ToShortDateString() : model?.Value;
                         return new DateEdit()
                         {
+                            Name = sysName,
                             EditValue = dt,
                             Text = dt,
                             Mask = "d",
@@ -104,15 +107,15 @@ namespace B6CRM.ViewModels.AdditionalFields
                             MaskType = MaskType.DateTime
                         };
 
-                    case "datetime": return new DateEdit() { DisplayTextConverter = new DateToStringConverter(), EditValue = value, Mask = "G", StyleSettings = new DateEditNavigatorWithTimePickerStyleSettings(), MaskCulture = CultureInfo.CurrentCulture, Text = value };
-                    case "percent": return new TextEdit() { Mask = "p", MaskType = MaskType.Numeric, EditValue = value };
-                    default: return new TextEdit() { EditValue = value };
+                    case "datetime": return new DateEdit() { DisplayTextConverter = new DateToStringConverter(), EditValue = model?.Value, Name = sysName, Mask = "G", StyleSettings = new DateEditNavigatorWithTimePickerStyleSettings(), MaskCulture = CultureInfo.CurrentCulture, Text = model?.Value };
+                    case "percent": return new TextEdit() { Mask = "p", MaskType = MaskType.Numeric, EditValue = model?.Value , Name = sysName };
+                    default: return new TextEdit() { EditValue = model?.Value, Name = sysName };
                 }
             }
             catch (Exception e)
             {
                 Log.ErrorHandler(e);
-                return new TextEdit() { EditValue = value };
+                return new TextEdit() { EditValue = model?.Value, Name = sysName };
             }
 
         }
@@ -123,20 +126,27 @@ namespace B6CRM.ViewModels.AdditionalFields
             {
                 foreach (var i in Fields)
                 {
-                    var value = db.AdditionalClientValue.Include(f => f.AdditionalField)
-                        .FirstOrDefault(
-                        f => f.AdditionalField.Label.ToString() == i.Label.ToString() 
-                         && 
-                         f.ClientId == client.Id
-                        );
-                    var val = ((BaseEdit)i.Content).EditValue?.ToString();
-
-                    if (value == null && val != null)
+                    if (i.Content is BaseEdit el)
                     {
-                        var item = new AdditionalClientValue() { ClientId = client.Id, Value = val, AdditionalFieldId = db.AdditionalClientFields.FirstOrDefault(f => f.Label.ToString() == i.Label.ToString()).Id };
-                        db.AdditionalClientValue.Add(item);
+                        var val = el.EditValue?.ToString();
+                        var sysName = el.Name?.ToString();
+
+
+                        var value = db.AdditionalClientValue.Include(f => f.AdditionalField).Where(
+                            f => f.AdditionalField.SysName == sysName && f.ClientId == client.Id).FirstOrDefault();
+
+                        if (value == null && val != null)
+                        {
+                            var item = new AdditionalClientValue() 
+                            {
+                                ClientId = client.Id, 
+                                Value = val, 
+                                AdditionalFieldId = db.AdditionalClientFields.FirstOrDefault(f => f.SysName == sysName).Id 
+                            };
+                            db.AdditionalClientValue.Add(item);
+                        }
+                        if (value != null) value.Value = val;
                     }
-                    if (value != null) value.Value = val;
                 }
             }
             catch (Exception e)
