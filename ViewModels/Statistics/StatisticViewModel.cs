80a574b57001d30ff69c1dff712d5bef6737c671
Balabin79 <50448295+Balabin79@users.noreply.github.com>
2023-04-30 13:07:53 +0400

    раздел статистики переделал

---
+++ ViewModels/Statistics/StatisticViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.DataAnnotations;
+
+
+namespace B6CRM.ViewModels.Statistics
+{
+    public class StatisticViewModel : ViewModelBase
+    {
+        public StatisticViewModel() 
+        {
+            Context = new LineViewModel();
+            UserControlName = "ProfitControl";
+        }      
+
+        [Command]     // Общий доход
+        public void Profit() => SetUserControl("ProfitControl");
+
+        [Command]     // Доход по сотрудникам
+        public void ProfitByEmployees() => SetUserControl("ProfitByEmployeesControl");
+
+        [Command]     // По рекламным источникам
+        public void Advertisings() => SetUserControl("AdvertisingsControl");
+
+        private void SetUserControl(string userControlName = "ProfitControl")
+        {
+            //включаем видимость контрола и подчеркиваем кнопку активного контрола
+            switch (userControlName)
+            {
+                case "ProfitControl":
+                    Context = new LineViewModel();
+                    UserControlName = userControlName;
+                    break;
+                case "ProfitByEmployeesControl":
+                    Context = new ProfitByEmployeesViewModel();
+                    UserControlName = userControlName;
+                    break;
+                case "AdvertisingsControl":
+                    Context = new AdvertisingSourcesViewModel();
+                    UserControlName = userControlName;
+                    break;
+            }
+        }
+
+        public string UserControlName
+        {
+            get { return GetProperty(() => UserControlName); }
+            set { SetProperty(() => UserControlName, value); }
+        }
+
+        public object Context
+        {
+            get { return GetProperty(() => Context); }
+            set { SetProperty(() => Context, value); }
+        }
+    }
+}
